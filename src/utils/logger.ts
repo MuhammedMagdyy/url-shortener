422837803f911d9b1100be512cd064b321162c3a
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-03-15 13:25:31 +0200

    feat: add Winston logging functionality

---
+++ src/utils/logger.ts
@@ -0,0 +1,36 @@
+import winston from 'winston';
+
+winston.addColors({
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'magenta',
+  debug: 'white',
+});
+
+const format = winston.format.combine(
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  winston.format.colorize({ all: true }),
+  winston.format.printf(
+    ({ timestamp, level, message, meta }) =>
+      `${timestamp} [${level}]: ${message}${meta ? ` ${JSON.stringify(meta)}` : ''}`,
+  ),
+);
+
+const logger = winston.createLogger({
+  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+  transports: [
+    new winston.transports.Console({ format }),
+    new winston.transports.File({
+      filename: 'logs/error.log',
+      level: 'error',
+      format: winston.format.json(),
+    }),
+    new winston.transports.File({
+      filename: 'logs/all.log',
+      format: winston.format.json(),
+    }),
+  ],
+});
+
+export { logger };
