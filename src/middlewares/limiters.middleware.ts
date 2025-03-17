fab94e5869dfe6defddeb3c3b2e48cf61b287822
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-03-17 13:37:58 +0200

    feat: enhance rate limiting middleware with custom error handling

--- src/middlewares/limiters.middleware.ts
+++ src/middlewares/limiters.middleware.ts
@@ -1,7 +1,15 @@
 import { rateLimit } from 'express-rate-limit';
+import { TOO_MANY_REQUESTS } from '../utils';
 
 export const limiter = rateLimit({
   windowMs: 2 * 60 * 1000,
   max: 5,
   message: 'Too many requests from this IP, please try again after 2 minutes',
+  handler: (_req, res, _next, options) => {
+    res.status(TOO_MANY_REQUESTS).json({
+      error: 'Rate limit exceeded',
+      details: options.message,
+      retryAfter: `${options.windowMs / 1000} seconds`,
+    });
+  },
 });
